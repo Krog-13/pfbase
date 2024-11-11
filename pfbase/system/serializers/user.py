2f08698b3fe435aedcfdc0ff5e81cba82b81c58e
Krog-13 <virtualspook@gmail.com>
2024-11-11 16:52:38 +0500

    serializer  - delete is_blocked

--- pfbase/system/serializers/user.py
+++ pfbase/system/serializers/user.py
@@ -22,7 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = 'last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email', \
-            'is_staff', 'is_active', 'date_joined', 'avatar', 'is_blocked', 'organization', 'groups'
+            'is_staff', 'is_active', 'date_joined', 'avatar', 'organization', 'groups'
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
