fd2f6c7c8c53f5c1990f6658c393cab408f71ffa
krog <virtualspook@gmail.com>
2024-12-08 14:45:00 +0500

    filter modification

--- pfbase/document/manager.py
+++ pfbase/document/manager.py
@@ -12,60 +12,11 @@ class RecordsManager(models.Manager):
         output = self.findByQuery(params)
         return output
 
-    def findByQueryOld(self, params):
-        """
-        Поиск по параметрам
-        """
-        quryset = self.all()
-        for key, value in params.items():
-            if key == "CODE":
-                quryset = quryset.filter(document__code=value)
-            if key == "number":
-                quryset = quryset.filter(number=value)
-            if key == "record_date":
-                quryset = quryset.filter(date__date=value)
-            if key == IndicatorType.STRING:
-                quryset = quryset.filter(record_values__value_str__contains=value)
-            if key == IndicatorType.INTEGER:
-                quryset = quryset.filter(record_values__value_int=value)
-            if key == IndicatorType.FLOAT:
-                quryset = quryset.filter(record_values__value_float=value)
-            if key == IndicatorType.BOOLEAN:
-                quryset = quryset.filter(record_values__value_bool=value)
-            if key == IndicatorType.DICTIONARY:
-                quryset = quryset.filter(record_values__value_reference=value)
-            if key == IndicatorType.DOCUMENT:
-                quryset = quryset.filter(record_values__value_reference=value)
-            if key == IndicatorType.LIST:
-                quryset = quryset.filter(record_values__value_reference=value)
-            if key == IndicatorType.DATETIME:
-                # ToDo: сравниваем только date and time без микросекунд
-                give_date_format(value, IndicatorType.DATETIME)
-                quryset = quryset.filter(record_values__value_datetime=value)
-            if key == IndicatorType.DATE:
-                quryset = quryset.filter(record_values__value_datetime__date=value)
-            if key == IndicatorType.TIME:
-                # ToDo: сравниваем только время без микросекунд
-                quryset = quryset.filter(record_values__value_datetime__time=value)
-            if key == IndicatorType.TEXT:
-                quryset = quryset.filter(record_values__value_text=value)
-            if key == IndicatorType.JSON:
-                quryset = quryset.filter(record_values__value_json=value)
-
-            if key == "STATUS":
-                quryset = quryset.annotate(
-                    latest_status_date=Max('history__created_at')
-                ).filter(
-                    history__status_id__in=value,
-                    history__created_at=F('latest_status_date')
-                )
-        return quryset
-
     def findByQuery(self, params):
         queryset = self.all()
         for key, value in params.items():
             if key == "author_id":
-                queryset = queryset.filter(author_id=value)
+                queryset = queryset.filter(author_id__in=value)
             if key == "NUMBER":
                 queryset = queryset.filter(number=value)
             if key == "DCM_CODE":
