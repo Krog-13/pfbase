f40d55c4cba77c7af618c1addb237e681b7becef
krog <virtualspook@gmail.com>
2025-01-19 15:35:42 +0500

    added pagination  - for organization

--- pfbase/system/views/organization.py
+++ pfbase/system/views/organization.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from pfbase.pagination import CustomPagination
 from ..serializers.organization import OrganizationSerializer, OrganizationCreateSerializer
 from ..models.organization import Organization
+from rest_framework.response import Response
 
 
 class OrganizationAPIView(ModelViewSet):
@@ -18,3 +19,14 @@ class OrganizationAPIView(ModelViewSet):
         if self.action == 'create':
             return OrganizationCreateSerializer  # Serializer used for POST
         return OrganizationSerializer
+
+    def list(self, request, *args, **kwargs):
+        no_page = request.query_params.get('paginate', 'true').lower() == 'false'
+
+        if no_page:
+            # Return all organizations without pagination
+            queryset = self.filter_queryset(self.get_queryset())
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+
+        return super().list(request, *args, **kwargs)
