725d5a1261107ff6a40c2d9b1a18bdd01d5ec88c
krog <virtualspook@gmail.com>
2025-02-19 16:24:53 +0500

    notification  - update

--- pfbase/system/serializers/notification.py
+++ pfbase/system/serializers/notification.py
@@ -3,6 +3,9 @@ from ..models.notification import Notification
 
 
 class NotificationSerializer(serializers.ModelSerializer):
+    read_at = serializers.DateTimeField(format="%d.%m.%Y %H:%M")
+    created_at = serializers.DateTimeField(format="%d.%m.%Y %H:%M")
+
     class Meta:
         model = Notification
         fields = "__all__"
