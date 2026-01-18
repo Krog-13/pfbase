6f4c6c5c0d2294847d84ac3f1e28a9f7c287771b
alishern <nalisher.2510@gmail.com>
2026-01-18 20:12:19 +0500

    1. Changed some validation file methods. Errors now in english 2. Added filetype library instead of python magic 3. Changed FileSaveSerializer and FilesSaveSerializer to work properly

--- pfbase/system/service.py
+++ pfbase/system/service.py
@@ -1,9 +1,8 @@
-import uuid
-
 from django.core.mail import send_mail
 import os
 from rest_framework import serializers
 import magic
+import filetype
 
 class EmailSendNotification:
     """
@@ -16,14 +15,22 @@ class EmailSendNotification:
 
 
 ALLOWED_EXTENSIONS = ("jpg", "jpeg", "png", "pdf", "docx", "xlsx", "txt")
-EXTENSION_TO_MIME = {
-    "jpg": ["image/jpeg"],
-    "jpeg": ["image/jpeg"],
-    "png": ["image/png"],
-    "pdf": ["application/pdf"],
-    "docx": ["application/vnd.openxmlformats-officedocument.wordprocessingml.document"],
-    "xlsx": ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"],
-    "txt": ["text/plain", "text/x-plain"],
+# EXTENSION_TO_MIME = {
+#     "jpg": ["image/jpeg"],
+#     "jpeg": ["image/jpeg"],
+#     "png": ["image/png"],
+#     "pdf": ["application/pdf"],
+#     "docx": ["application/vnd.openxmlformats-officedocument.wordprocessingml.document"],
+#     "xlsx": ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"],
+#     "txt": ["text/plain", "text/x-plain"],
+# }
+EXTENSION_TO_FILETYPE = {
+    "jpg": ["jpg"],
+    "jpeg": ["jpg"],
+    "png": ["png"],
+    "pdf": ["pdf"],
+    "docx": ["zip"],
+    "xlsx": ["zip"],
 }
 MAX_SIZE_BY_EXTENSION = {
     "jpg": 20 * 1024 * 1024,
@@ -42,61 +49,109 @@ class FileValidationService:
         cls.validate_size(file)
         cls.validate_filename(file)
         extension = cls.validate_extension(file)
-        detected_mime = cls.detect_real_mime(file)
-        cls.validate_mime_vs_extension(extension, detected_mime)
-        cls.dangerous_format_extensions(extension)
+        detected_type = cls.detect_real_type(file)
+        cls.validate_type_vs_extension(file, extension, detected_type)
+        # detected_mime = cls.detect_real_mime(file)
+        # cls.validate_mime_vs_extension(extension, detected_mime)
         return file
 
     @classmethod
     def validate_size(cls, file):
         if file.size == 0:
-            raise serializers.ValidationError("Файл пустой")
+            raise serializers.ValidationError({
+                "file": file.name,
+                "error": "File is empty"
+            })
 
         extension = cls._get_extension(file)
         max_size = MAX_SIZE_BY_EXTENSION.get(extension)
 
         if max_size and file.size > max_size:
-            raise serializers.ValidationError(f"Файл слишком большой. Максимум {max_size // (1024 * 1024)} MB")
+            raise serializers.ValidationError({
+                "file": file.name,
+                "error": f"File is too large. Maximum {max_size // (1024 * 1024)} MB"
+            })
 
     @classmethod
     def validate_filename(cls, file):
         name = file.name
 
         if len(name) > 255:
-            raise serializers.ValidationError("Слишком длинное имя файла")
+            raise serializers.ValidationError({
+                "file": file.name,
+                "error": "File name too long"
+            })
 
         for part in FORBIDDEN_FILENAME_PARTS:
             if part in name:
-                raise serializers.ValidationError("Недопустимое имя файла")
+                raise serializers.ValidationError({
+                    "file": file.name,
+                    "error": "Invalid file name"
+                })
 
     @classmethod
     def validate_extension(cls, file):
         extension = cls._get_extension(file)
 
         if extension not in ALLOWED_EXTENSIONS:
-            raise serializers.ValidationError(
-                f"Недопустимое расширение файла: .{extension}."
-                f"Разрешены {', '.join(ALLOWED_EXTENSIONS)}"
-            )
+            raise serializers.ValidationError({
+                "file": file.name,
+                "error": f"Invalid file extension: .{extension}. "
+                         f"Allowed extensions {', '.join(ALLOWED_EXTENSIONS)}"
+            })
 
         return extension
 
+    # @classmethod
+    # def detect_real_mime(cls, file):
+    #     header = file.read(2048)
+    #     file.seek(0)
+    #
+    #     mime = magic.from_buffer(header, mime=True)
+    #     return mime
+
+    # @classmethod
+    # def validate_mime_vs_extension(cls, extension, detected_mime):
+    #     allowed_mimes = EXTENSION_TO_MIME.get(extension)
+    #
+    #     if not allowed_mimes:
+    #         raise serializers.ValidationError(f"Неизвестный тип файла. Разрешены {','.join(ALLOWED_EXTENSIONS)} типы файлов")
+    #     if detected_mime not in allowed_mimes:
+    #         raise serializers.ValidationError(f"Тип файла не соответствует расширению ({detected_mime})")
+
     @classmethod
-    def detect_real_mime(cls, file):
-        header = file.read(2048)
+    def detect_real_type(cls, file):
+        header = file.read(261)
         file.seek(0)
 
-        mime = magic.from_buffer(header, mime=True)
-        return mime
+        kind = filetype.guess(header)
+
+        if kind is None:
+            raise serializers.ValidationError({
+                "file": file.name,
+                "error": "Unable to determine file type"
+            })
+
+        return kind.extension
 
     @classmethod
-    def validate_mime_vs_extension(cls, extension, detected_mime):
-        allowed_mimes = EXTENSION_TO_MIME.get(extension)
+    def validate_type_vs_extension(cls, file, extension, detected_type):
+        if extension == 'txt':
+            return
 
-        if not allowed_mimes:
-            raise serializers.ValidationError(f"Неизвестный тип файла. Разрешены {','.join(ALLOWED_EXTENSIONS)} типы файлов")
-        if detected_mime not in allowed_mimes:
-            raise serializers.ValidationError(f"Тип файла не соответствует расширению ({detected_mime})")
+        allowed_types = EXTENSION_TO_FILETYPE.get(extension)
+
+        if not allowed_types:
+            raise serializers.ValidationError({
+                "file": file.name,
+                "error": "Unknown file type"
+            })
+
+        if detected_type not in allowed_types:
+            raise serializers.ValidationError({
+                "file": file.name,
+                "error": f"The file type does not match the extension {detected_type}"
+            })
 
     @staticmethod
     def _get_extension(file):
